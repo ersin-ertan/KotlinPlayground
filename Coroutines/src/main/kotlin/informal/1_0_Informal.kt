2aebd6118dea8e6d45ce128552c76179c298e202
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-08-08 23:14:50 -0400

    Initial commit

---
+++ Coroutines/src/main/kotlin/informal/1_0_Informal.kt
@@ -0,0 +1,11 @@
+package informal/*
+* https://github.com/Kotlin/kotlin-coroutines/blob/master/kotlin-coroutines-informal.md
+*
+* What - generators/yield, async/await, composable/delimited continuations
+*
+* Goals - possible to use coroutines as wrappers for different existing async apis
+* */
+
+fun test(){
+
+}
