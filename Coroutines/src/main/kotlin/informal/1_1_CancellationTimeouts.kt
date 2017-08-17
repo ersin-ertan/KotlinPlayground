f37146cec7c6e1b9fe3a2b79fcb26a666d4a0546
Ersin Ertan <4809853+ersin-ertan@users.noreply.github.com>
2017-08-16 20:51:05 -0400

    Running non cancellable blocks, timeout with catch

--- Coroutines/src/main/kotlin/informal/1_1_CancellationTimeouts.kt
+++ Coroutines/src/main/kotlin/informal/1_1_CancellationTimeouts.kt
@@ -1,9 +1,6 @@
 package informal
 
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.*
 
 fun cancellingExecution() = runBlocking{
 // large applications need finer-grained control of cancellation, launch returns a Job to be cancelled
@@ -42,12 +39,13 @@ fun cooperativeCancellation() = runBlocking{
     println("quitting")
 }
 
+fun computationCodeCancellable() = runBlocking<Unit> {
 // there are two ways to make computation cancellable, periodically invoking suspending function, yield(), or
 // explicitly checking the cancellation status
-fun computationCodeCancellable() = runBlocking<Unit> {
     val job = launch(CommonPool) {
         var nextPrintTime = 0L
         var i = 0
+                // how often is this checked?
         while (isActive) { // cancellable computation loop, isActive is a property inside coroutines via CoroutineScope
             val currentTime = System.currentTimeMillis()
             if (currentTime >= nextPrintTime) {
@@ -63,31 +61,116 @@ fun computationCodeCancellable() = runBlocking<Unit> {
     println("main: see no more output, now I can quit.")
 }
 
-    // cacellable suspending functions throw CanccellationException on cancellation, which can be handled with try catch
-// and kotlin's use function executions for finalization when cancelled
 fun closingResourcesWithFinally() = runBlocking<Unit> {
-        val job = launch(CommonPool) {
-            try {
-                repeat(1000) { i ->
-                    println("I'm sleeping $i ...")
-                    delay(500L)
-                }
-            } finally {
-                println("I'm running, but job.cancel() has been called")
+// cancellable suspending functions throw CanccellationException on cancellation, which can be handled with try catch
+// and kotlin's use function executions for finalization when cancelled
+    val job = launch(CommonPool) {
+        try {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+        } finally {
+            println("I'm running, but job.cancel() has been called")
+        }
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancel() // cancels the job
+    delay(1300L) // delay a bit to ensure it was cancelled indeed
+    println("main: Now I can quit.")
+}
+
+fun runNonCancellableBlock() = runBlocking{
+    // suspending functions in the finally block will throw a CancellationException, since the coroutine running the code
+    // is cancelled. Usually closing a file, cancelling job, closing a channel, are non blocking. But a rare case may require
+    // a suspend, thus wrap it in run(nonCancellable){}
+
+    val job = launch(CommonPool){
+        try {
+            repeat(1000){ i ->
+                println("sleeping $i...")
+                delay(400L)
+            }
+        } finally {
+            run(NonCancellable){
+                println("running non cancellable")
+                delay(1000L) // delay is only allowed in coroutines
+                println("delayed for 1 sec in non cancellable code")
             }
         }
-        delay(1300L) // delay a bit
-        println("main: I'm tired of waiting!")
-        job.cancel() // cancels the job
-        delay(1300L) // delay a bit to ensure it was cancelled indeed
-        println("main: Now I can quit.")
+    }
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!, cancelling")
+    job.cancel() // cancels the job
+    delay(1300L) // delay a bit to ensure it was cancelled indeed
+    println("main: Now I can quit.")
 }
+
+fun timeout()=runBlocking{
+    // will throw TimeoutException, by withTimeout, a private subclass of CancellaionException. so use try catch
+    try {
+        withTimeout(1200L){
+            repeat(1000){i ->
+                println("sleeping $i...")
+                delay(400L)
+            }
+        }
+    }catch (ce:CancellationException){ println("caught cancellationException")}
+}
+
 
 fun main(args: Array<String>) {
 //    cancellingExecution()
 //    cooperativeCancellation()
 //    computationCodeCancellable()
 //    closingResourcesWithFinally()
-
-
+//    runNonCancellableBlock()
+//    timeout()
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
